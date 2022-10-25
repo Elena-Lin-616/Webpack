005b6c5cc60db18340225a43355fbd62050446a3
Na Lin <lna483018@gmail.com>
2022-10-24 21:20:37 -0400

    recap how to implement webpack

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import helloWorld from "./hello-world.js";
-import addImage from "./add-image.js";
+// import addImage from "./add-image.js";
 helloWorld();
 console.log("1st complete of webpack");
 // * each time when I rebuild js app by webpace, auto refresh webpage
 
 // todo : learn how to use asset module to import different files
-addImage();
+// addImage();
